771a58b2237b40e756486989a32a9b14b263668c
Aswin Asok <aswinasokofficial@gmail.com>
2025-11-22 19:43:04 +0530

    fix(build)

--- app/components/DetectionMethods.tsx
+++ app/components/DetectionMethods.tsx
@@ -52,7 +52,7 @@ export default function DetectionMethods() {
             y: 0,
             transition: {
                 duration: 0.5,
-                ease: [0.25, 0.1, 0.25, 1],
+                ease: [0.25, 0.1, 0.25, 1] as const,
             },
         },
     };
